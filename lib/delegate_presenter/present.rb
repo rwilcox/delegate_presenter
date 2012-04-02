89103248f20cae3ebc4a4ecfe203cc5c34eade29
Jordan Babe <=>
2012-04-02 13:28:43 -0600

    so that you can overwrite default presenter class name

--- lib/delegate_presenter/present.rb
+++ lib/delegate_presenter/present.rb
@@ -1,4 +1,5 @@
-def Present(object)
-  presenter_class = Object.const_get("#{object.class.name}Presenter")
+def Present(object, opts = {})
+  klass = opts[:with] || "#{object.class.name}Presenter"
+  presenter_class = Object.const_get(klass)
   presenter_class.new(object)
 end
